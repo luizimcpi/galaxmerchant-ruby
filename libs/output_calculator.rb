da628813ecc1978d80fcbf7a4519a61f8c85d035
Luiz Henrique <luizhenrique.se@gmail.com>
2018-07-19 21:52:54 -0300

    Tentativa de resolução da calculadora

--- libs/output_calculator.rb
+++ libs/output_calculator.rb
@@ -1,8 +1,22 @@
 class Calculator
 	def sum_coin_symbols(coin_array)
+		result = ""
+		sum = 0
 		coin_array.each { |coin|
-			puts coin
+			if coin.eql?("glob")
+				sum = sum + 1
+			end
+			if coin.eql?("prok")
+				sum = sum + 5
+			end
+			if coin.eql?("pish")
+				sum = sum + 10
+			end
+			if coin.eql?("tegj")
+				sum = sum + 50
+			end
 		}
-		return "42"
+		result = sum.to_s
+		return result
 	end
 end
