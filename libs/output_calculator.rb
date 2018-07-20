7294765fe31049a00e46d9f7f2b1ee661877aa1b
Luiz Evangelista <luiz.evangelista@concrete.com.br>
2018-07-20 14:58:20 -0300

    Cria metodo para verificação de repetição

--- libs/output_calculator.rb
+++ libs/output_calculator.rb
@@ -2,21 +2,23 @@ require_relative './exceptions/sequence_number_exception'
 require_relative './model/number'
 
 class Calculator
+	
 	def self.sum_coin_symbols(coin_array)
 		result = ""
 		i = 0
 		sum = 0
-		last_number=0
+		last_number = 0
 		number_one = Number.new(1)
 		number_five = Number.new(5)
 		number_ten = Number.new(10)
 		number_fifty = Number.new(50)
 		coin_array.each { |coin|
 			if coin.eql?("glob")
-				if last_number == number_one.value && number_one.sequence_times > 2
+				if is_number_repeated_more_than_three_times(number_one, last_number)
 					raise SequenceNumberError.new
 				end
-				if (last_number == number_one.value && number_one.sequence_times <= 2) || i == 0
+				
+				if (is_number_repeated_more_than_three_times(number_one, last_number) == false) || i == 0
 					number_one.increment_sequence_times
 					sum = sum + number_one.value
 					last_number = number_one.value
@@ -31,10 +33,10 @@ class Calculator
 				number_ten.default_sequence_times
 			end
 			if coin.eql?("pish")	
-				if last_number == number_ten.value && number_ten.sequence_times > 2
+				if is_number_repeated_more_than_three_times(number_ten, last_number)
 					raise SequenceNumberError.new
 				end
-				if (last_number == number_ten.value && number_ten.sequence_times <= 2) || i == 0
+				if (is_number_repeated_more_than_three_times(number_ten, last_number) == false) || i == 0
 					number_ten.increment_sequence_times
 					sum = sum + number_ten.value
 					last_number = number_ten.value
@@ -52,4 +54,12 @@ class Calculator
 		result = sum.to_s
 		return result
 	end
+
+	def self.is_number_repeated_more_than_three_times(number, last_number)
+		if last_number == number.value && number.sequence_times > 2
+			return true
+		end
+		return false
+	end
+
 end
